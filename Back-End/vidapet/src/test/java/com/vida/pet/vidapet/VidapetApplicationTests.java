d88bf1c972101a0ae653a4176bb938b37bd597d7
AlexCajeFelix <alexcaje2005@gmail.com>
2025-04-01 22:21:23 -0300

    feat: add docker and commit lint and commitzen

---
+++ Back-End/vidapet/src/test/java/com/vida/pet/vidapet/VidapetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vida.pet.vidapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidapetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
